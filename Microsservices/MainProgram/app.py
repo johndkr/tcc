2c2f27691628dc120b14bb62781eeb502138d589
Bruno Vasconcelos <brunohvlemos@gmail.com>
2019-06-19 12:40:10 -0300

    asdljk
    aksldajlk

--- Microsservices/MainProgram/app.py
+++ Microsservices/MainProgram/app.py
@@ -72,17 +72,75 @@ def my_form_post():
 							num_articles = newsInfo[3], sources = sources, authors = authors, titles = titles, descriptions = descriptions,
 							url_original = processed_text, fonte = source[0]["source_name"], prob_fake = round(fact_points*100,2), pb = Source_Checking.POLITICAL_BIAS[political_bias], sf = Source_Checking.FACTUALITY[source_factuality])
 	
+@app.route('/get_linguist_prob')
+def linguistic_page():
+	result = "Nenhum resultado ainda"
+	return render_template("linguistic.html", result = result)
+
 @app.route('/get_linguist_prob', methods=['POST'])
 def get_linguist_probability():
-    data = request.json
+    data = request.form['linguisticBox']
 
-    if (data['txt']):
-        result = ling.make_linguistic_analyses(data['txt'])
+    if (len(data)>0):
+        result = ling.make_linguistic_analyses(data)
     else:
         result = "Ops... there is no text to be analysed!"
 
-    return result
+    return render_template("linguistic.html", result = result)
 
     
+@app.route("/mock")
+def home_mock():
+	return render_template("exemplo_mock.html")
+
+@app.route('/mock', methods=['POST'])
+def my_form_post_mock():
+	text = request.form['newsLink']
+	processed_text = text
+
+	id_news = 0
+	newsInfo = ['','','','','']
+	keywords = 0
+	articles = 0
+	length = 0
+	sources = ['']
+	authors = ['']
+	titles = ['']
+	descriptions = ['']
+	source = [{"source_name": ""}]
+	fact_points = 0
+	political_bias = 0
+	source_factuality = 0
+
+	link_mode = False
+
+	if is_url(processed_text):
+
+		processed_url = Source_Checking.extractWebsite(processed_text)
+		print(processed_url)
+		source = Source_Checking.querySourceByUrl(processed_url)
+		political_bias = source[0]["political_bias"]
+		source_factuality = source[0]["factuality"]
+
+		fact_points = 0.5*Parameters.POLITICAL_BIAS_BASE[str(political_bias)] + 0.5*Parameters.FACTUALITY_BASE[str(source_factuality)]
+		newsInfo[1] = fact_points
+		sources[0] = processed_url
+
+		link_mode = True
+	else:
+		id_news = Main.getNews(processed_text)
+		newsInfo = Main.getNewsInfo(id_news)
+		keywords = ",".join(str(x) for x in newsInfo[2])
+		articles = newsInfo[4]
+		length = len(articles)
+		sources = [item['source']['name'] for item in articles]
+		authors = [item['author'] for item in articles]
+		titles = [item['title'] for item in articles]
+		descriptions = [item['description'] for item in articles]
+
+	return render_template("index_mock.html", link_mode = link_mode, length = length, id_news = newsInfo[0], fake_status = newsInfo[1], keywords = keywords, 
+							num_articles = newsInfo[3], sources = sources, authors = authors, titles = titles, descriptions = descriptions,
+							url_original = processed_text, fonte = source[0]["source_name"], prob_fake = round(fact_points*100,2), pb = Source_Checking.POLITICAL_BIAS[political_bias], sf = Source_Checking.FACTUALITY[source_factuality])
+
 if __name__ == "__main__":
 	app.run(debug=True)
---
+++ Microsservices/MainProgram/data/news_mock.py
@@ -0,0 +1,16 @@
+noticias_mock = {
+	"articles" = [
+		{
+			"fonte": "Twitter",
+			"url": "https://twitter.com/PresidenBr/status/1140424538196533249",
+			"perfil": "PresidenBr",
+			"texto": "The Intercept não provou que são verídicas nenhuma das supostas trocas de mensagens que divulgou. Porque agora tem gente preocupado com as provas do que foi divulgado pelo Pavão? #ShowDoPavão"
+		},
+		{
+			"fonte": "The Intercept",
+			"url": "https://theintercept.com/2019/06/09/chat-moro-deltan-telegram-lava-jato/",
+			"perfil": "",
+			"texto": "Sergio Moro e Deltan Dallagnol trocaram mensagens de texto que revelam que o então juiz federal foi muito além do papel que lhe cabia quando julgou casos da Lava Jato. Em diversas conversas privadas, até agora inéditas, Moro sugeriu ao procurador que trocasse a ordem de fases da Lava Jato, cobrou agilidade em novas operações, deu conselhos estratégicos e pistas informais de investigação, antecipou ao menos uma decisão, criticou e sugeriu recursos ao Ministério Público e deu broncas em Dallagnol como se ele fosse um superior hierárquico dos procuradores e da Polícia Federal."
+		},
+	]
+} 
