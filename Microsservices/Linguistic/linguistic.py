5b9cce95bd4af006c50c351b7f6421ae245c38ef
johndkr <joaomorales01@gmail.com>
2019-11-21 13:13:45 -0300

    adding feeling analysis in message and fixing some methods on linguistic

--- Microsservices/Linguistic/linguistic.py
+++ Microsservices/Linguistic/linguistic.py
@@ -9,8 +9,7 @@ import spacy
 import re
 
 from spellchecker import SpellChecker
-from unicodedata import normalize
-from pyUFbr.baseuf import ufbr
+from unicodedata import normalize as unicodedata
 from collections import Counter 
 from googletrans import Translator
 
@@ -24,8 +23,6 @@ from Microsservices.CommonUtil.Log import log_util
 
 ## consult this before defining which word split will be used: https://machinelearningmastery.com/clean-text-machine-learning-python/
 
-STATE_CITIES_DICTIONARY = ".\\data\\state_city_dictionary"
-
 class LinguisticAnalyses():
   spellchecker = SpellChecker()
   log_manager = log_util.Log_Util(True)
@@ -34,27 +31,6 @@ class LinguisticAnalyses():
   def __init__(self):
     self.spellchecker = SpellChecker()
     self.__load_known_words()
-    self.__states_keys_dictionary = self.__load_states_dic_keys()
-
-  def __load_states_dic_keys(self):
-    ##load states dictionary
-    self.log_manager.info('Loading states dictionary file...')
-    states_file_loaded = None
-    try:
-      path = os.path.join(os.path.dirname(os.path.abspath(__file__)), STATE_CITIES_DICTIONARY)
-      states_file_loaded = eval(open(path).read())
-      self.log_manager.info('Found file at: ' + path)
-    except Exception as err:
-      self.log_manager.exception(err)
-      states_file_loaded = {}
-    finally:
-      return states_file_loaded
-
-  def __text_cleanner(self, text):
-    self.log_manager.debbug("cleanning text: {}...".format(text[0:15]))
-    ### this method receives a dirty string and removes all its parameters
-    ### currently it only strips the ponctuation
-    return text.translate(str.maketrans('', '', string.punctuation))
   
   def __accent_remover(self, txt):
     ### this method removes all accents from a string
@@ -70,70 +46,11 @@ class LinguisticAnalyses():
   def __load_known_words(self):
     self.spellchecker.word_frequency.load_words(["Lula", "Bolsonaro"])
 
-  def top_n_words(self, n, text):
-    ## https://www.geeksforgeeks.org/find-k-frequent-words-data-set-python/
-    # split() returns list of all the words in the string 
-    split_it = self.__text_cleanner(text).lower().split()
-    
-    # Pass the split_it list to instance of Counter class. 
-    counter = Counter(split_it) 
-      
-    # most_common() produces k frequently encountered 
-    # input values and their respective counts. 
-
-    return counter.most_common(n)
-
   def wrong_proportion(self, text):
     text_cleanned = self.__accent_remover(text)
-    split_it = self.__text_cleanner(text_cleanned).lower().split()
+    split_it = self.__remove_them_all(text_cleanned).lower().split()
     return len(self.spellchecker.unknown(split_it))/(len(split_it))
   
-  # def catch_state_mentions(self, text, also_in_lower):
-  #   self.log_manager.info("Catching states for text {}".format(text[0:10]))
-    
-  #   found =[]
-  #   # text = self.__text_cleanner(text).upper()
-
-  #   for key in self.__states_keys_dictionary:
-  #     for city in self.__states_keys_dictionary[key]:
-  #       count = text.count(city)
-  #       if count != 0:
-  #         found.append((key, city, count))
-    
-  #   if(also_in_lower):
-  #     for key in self.__states_keys_dictionary:
-  #       for city in self.__states_keys_dictionary[key]:
-  #         count = text.count(city.lower())
-  #         if count != 0:
-  #           found.append((key, city, count))
-
-  #   return found
-
-  # def save_cities_learned(self):
-  #   ### updates saved dictionary with the current one loaded on memory
-  #   try:
-  #     path = os.path.join(__file__, STATE_CITIES_DICTIONARY)
-  #     with open(path, 'w') as file:
-  #       file.write(json.dumps(self.__states_keys_dictionary))
-  #   except Exception as err:
-  #     self.log_manager.exception(err)
-
-  # def learn_new_cities(self, txt):
-    ## To learn new cities mentioned we search for it comparing in upper case
-    
-    text_cleanned = self.__accent_remover(self.__text_cleanner(txt)).upper()
-
-    for state in ufbr.list_uf:
-      cities_normalized = [self.__accent_remover(city) for city in ufbr.list_cidades(state)]
-      for city in cities_normalized:
-        if city in text_cleanned:
-            # check if state is already known
-            if not state in self.__states_keys_dictionary:
-              self.__states_keys_dictionary[state] = []
-            self.__states_keys_dictionary[state].append(city.title())
-
-    self.save_cities_learned()
-
   def get_words_types(self, txt):
     self.log_manager.debbug("Getting words types...")
     
@@ -155,6 +72,24 @@ class LinguisticAnalyses():
 
     return types_count
 
+  def get_defined_word_types(self, txt):
+    word_types = self.get_words_types(txt)
+
+    result_json = {
+      "verbos": sum('VERB' in type_tuple for type_tuple in word_types),
+      "substantivos": sum('NOUN' or 'PROPN' in type_tuple for type_tuple in word_types),
+      "adverbios": sum('ADV' in type_tuple for type_tuple in word_types),
+      "pronomes": sum('PRON' in type_tuple for type_tuple in word_types),
+      "artigos": sum('DET' in type_tuple for type_tuple in word_types),
+      "adjetivo": sum('ADJ' in type_tuple for type_tuple in word_types),
+      "numerais": sum('NUM' in type_tuple for type_tuple in word_types),
+      "preposicoes": sum('ADP' in type_tuple for type_tuple in word_types),
+      "conjuncoes": sum('CCONJ' or 'SCONJ' in type_tuple for type_tuple in word_types),
+      "interjeicoes": 0
+    }
+
+    return result_json
+ 
   def count_words(self, txt):
     txt_sem_pontuacao = self.__remove_them_all(txt)
     return len(txt_sem_pontuacao.split())
@@ -167,7 +102,7 @@ class LinguisticAnalyses():
     doc = self.nlp(txt)
     return doc.ents
 
-  def get_all_entities(self, txt):
+  def get_all_entities_vector(self, txt):
     # this method receives a text and counts its entities
     # IT IS SENSITIVE to grammar mistakes on the text
 
@@ -231,21 +166,6 @@ class LinguisticAnalyses():
     words = clean_text.split()
     return sum(len(word) for word in words) / len(words)
 
-  def translate_pt_to_en(self, txt):
-    """ translates text to english """
-    translator = Translator()
-    result = translator.translate(txt, src='pt')
-    translator = None
-    return str(result.text)
-
-  def get_txt_feeling(self, txt):
-    self.log_manager.debbug("Getting text feeling")
-
-    txt_en = self.translate_pt_to_en(txt)
-    feeling = feeling_evaluator.get_text_feeling(txt_en)
-
-    return int(feeling[1][0])
-
   def __chances_based_word_type(self, types):
     verb = types['VERB'] if 'VERB' in types else 0
     noun = types['NOUN'] if 'NOUN' in types else 0
@@ -257,13 +177,11 @@ class LinguisticAnalyses():
       chance = (1/verb)*0.33 + (1/noun)*0.34 + (1/adv)*0.33
     return chance
 
-  def make_linguistic_analyses(self, txt):
+  def make_linguistic_analyses_old(self, txt):
     result = {
       "Proportion" : self.wrong_proportion(txt),
-      "Locations" : self.catch_state_mentions(txt, True),
       "WordsTypes" : self.count_words_types(txt),
       "Entities" : self.count_entities(txt),
-      "Feeling" : self.get_txt_feeling(txt)
     }
 
     prop_chance = result["Proportion"]/0.3
@@ -273,9 +191,15 @@ class LinguisticAnalyses():
     else:
       fake_new_chance = 1
 
-    result["FakeNewChance"] = fake_new_chance
+    result["FakeNewChance"] = fake_new_chance 
 
     return result
+
+  def make_linguistic_analyses(self, txt):
+    """ this method run all analysis for this module and return them within a json """
+
+    return {}
+
 
 if __name__ == "__main__":
     txt = "O Lula e o Machao de Assis foi solto ontem"
--- Microsservices/Message/message.py
+++ Microsservices/Message/message.py
@@ -1,1 +1,121 @@
 #message
+import os, sys
+sys.path.append('..\\..\\')
+
+from googletrans import Translator
+from unicodedata import normalize as unicodedata
+from collections import Counter 
+from pyUFbr.baseuf import ufbr
+
+from Microsservices.Linguistic import feeling_evaluator 
+from Microsservices.CommonUtil.Log import log_util
+
+import re
+import nltk
+import json
+
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+
+STATE_CITIES_DICTIONARY = ".\\data\\state_city_dictionary"
+
+class MessageAnalyses():
+    log_manager = log_util.Log_Util(True)
+
+    def __init__(self):
+        self.__states_keys_dictionary = self.__load_states_dic_keys()
+
+    def __remove_them_all(self, txt):
+        # This method returns a string only containg numbers, letters and space
+        nfkd = unicodedata.normalize('NFKD', txt)
+        palavraSemAcento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
+
+        return re.sub('[^a-zA-Z0-9 \\\]', '', palavraSemAcento)
+
+    def __load_states_dic_keys(self):
+        ##load states dictionary
+        self.log_manager.info('Loading states dictionary file...')
+        states_file_loaded = None
+        try:
+            path = os.path.join(os.path.dirname(os.path.abspath(__file__)), STATE_CITIES_DICTIONARY)
+            states_file_loaded = eval(open(path).read())
+            self.log_manager.info('Found file at: ' + path)
+        except Exception as err:
+            self.log_manager.exception(err)
+            states_file_loaded = {}
+        finally:
+            return states_file_loaded
+
+    def translate_pt_to_en(self, txt):
+        """ translates text to english """
+        translator = Translator()
+        result = translator.translate(txt, src='pt')
+        translator = None
+        return str(result.text)
+
+    def get_txt_feeling(self, txt):
+        self.log_manager.debbug("Getting text feeling")
+
+        txt_en = self.translate_pt_to_en(txt)
+        feeling = feeling_evaluator.get_text_feeling(txt_en)
+
+        return int(feeling[1][0])
+
+    def top_n_words(self, n, text):
+        ## https://www.geeksforgeeks.org/find-k-frequent-words-data-set-python/
+        # split() returns list of all the words in the string 
+        split_it = self.__remove_them_all(text).lower().split()
+        
+        # Pass the split_it list to instance of Counter class. 
+        counter = Counter(split_it) 
+        
+        # most_common() produces k frequently encountered 
+        # input values and their respective counts. 
+
+        return counter.most_common(n)
+
+    def catch_state_mentions(self, text, also_in_lower):
+        self.log_manager.info("Catching states for text {}".format(text[0:10]))
+        
+        found =[]
+        # text = self.__text_cleanner(text).upper()
+
+        for key in self.__states_keys_dictionary:
+            for city in self.__states_keys_dictionary[key]:
+                count = text.count(city)
+                if count != 0:
+                    found.append((key, city, count))
+        
+        if(also_in_lower):
+            for key in self.__states_keys_dictionary:
+                for city in self.__states_keys_dictionary[key]:
+                    count = text.count(city.lower())
+                    if count != 0:
+                        found.append((key, city, count))
+
+        return found
+
+    def save_cities_learned(self):
+        ### updates saved dictionary with the current one loaded on memory
+        try:
+            path = os.path.join(__file__, STATE_CITIES_DICTIONARY)
+            with open(path, 'w') as file:
+                file.write(json.dumps(self.__states_keys_dictionary))
+        except Exception as err:
+            self.log_manager.exception(err)
+
+    def learn_new_cities(self, txt):
+        # To learn new cities mentioned we search for it comparing in upper case
+        
+        text_cleanned = self.__remove_them_all(txt).upper()
+
+        for state in ufbr.list_uf:
+            cities_normalized = [self.__remove_them_all(city) for city in ufbr.list_cidades(state)]
+            for city in cities_normalized:
+                if city in text_cleanned:
+                    # check if state is already known
+                    if not state in self.__states_keys_dictionary:
+                        self.__states_keys_dictionary[state] = []
+                        self.__states_keys_dictionary[state].append(city.title())
+
+        self.save_cities_learned()
